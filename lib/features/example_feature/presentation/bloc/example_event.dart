8f775916fbe87dd64ba8fd47543d2e2bf5d184cc
Houmed <temp44688@gmail.com>
2026-08-27 04:07:06 +0300

    Refactor ExampleEvent to extend Equatable

--- lib/features/example_feature/presentation/bloc/example_event.dart
+++ lib/features/example_feature/presentation/bloc/example_event.dart
@@ -1,7 +1,17 @@
-sealed class ExampleEvent {
+import 'package:equatable/equatable.dart';
+
+abstract class ExampleEvent extends Equatable {
   const ExampleEvent();
+
+  @override
+  List<Object?> get props => [];
 }
 
-class GetExampleRequested extends ExampleEvent {
-  const GetExampleRequested();
+class ExampleLoadRequested extends ExampleEvent {
+  const ExampleLoadRequested(this.id);
+
+  final String id;
+
+  @override
+  List<Object?> get props => [id];
 }
